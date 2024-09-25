100a94b215c215a4a16feb3485d9bf5fbe030f99
Konstantin Dryagin <123456789+odin@users.noreply.github.com>
2024-09-25 17:21:30 +0300

    fix: Глобально переделал код
    Все переменные теперь внутренние а не глобальные.
    Весь код теперь в одной функции.
    Проверка в функции, что человек победил теперь
    происходит через объект.
    Функция getComputerChoise() теперь сразу передает
    значение выбора компьютера.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function getComputerChoise() {
-    return Math.floor(Math.random() * 3)
+    const arrayObjectGame = ["ножницы", "бумага", "камень"]
+    return arrayObjectGame[Math.floor(Math.random() * 3)]
 }
 
 function getHumanChoice() {
-    let getHumaenAnswer = prompt("Это игра камень,ножницы,бумага. Введите одно из трех слов - ножницы,бумага,камень. Всего 5 итераций.")
-    getHumaenAnswer = getHumaenAnswer.toLowerCase()
-    if (getHumaenAnswer == "ножницы" || getHumaenAnswer == "бумага" || getHumaenAnswer == "камень") {
-        return getHumaenAnswer
+    let getHumanAnswer = prompt("Это игра камень,ножницы,бумага. Введите одно из трех слов - ножницы,бумага,камень. Всего 5 итераций.")
+    getHumanAnswer = getHumanAnswer.toLowerCase()
+    if (getHumanAnswer == "ножницы" || getHumanAnswer == "бумага" || getHumanAnswer == "камень") {
+        return getHumanAnswer
     }
     else {
         alert("Вы ввели не валидные данные")
@@ -15,45 +16,48 @@ function getHumanChoice() {
 
 }
 
-let humenScore = 0;
-let computerScore = 0;
-const computerArray = ["ножницы", "бумага", "камень"]
 function playRound() {
-    let humenChoise = getHumanChoice()
-    let computerChoise = getComputerChoise()
-    computerChoise = computerArray[computerChoise];
-    if (humenChoise == computerChoise) {
-        alert("Ничья")
-        console.log("Ничья")
+    const winsChoice = {
+        "бумага" : ["камень"],
+        "ножницы" : ["бумага"],
+        "камень" : ["ножницы"]
     }
-    else if (humenChoise == "ножницы" && computerChoise == "камень" ||
-        humenChoise == "бумага" && computerChoise == "ножницы" ||
-        humenChoise == "камень" && computerChoise == "бумага") {
-        humenScore--;
-        computerScore++
-        console.log("Вас унизил компьютер, ваш счет: " + humenScore + " и счет компьютера: " + computerScore + " ")
-        alert("Вас унизил компьютер, ваш счет: " + humenScore + " и счет компьютера: " + computerScore + " ")
+    let humanChoise = getHumanChoice()
+    let computerChoise = getComputerChoise()
+    if (humanChoise == computerChoise) {
+        alert("Ничья!")
+        return ""
+    }
+    if (winsChoice[humanChoise].includes(computerChoise)) {
+        return "humanWin"
     }
     else {
-        humenScore++
-        computerScore--
-        console.log("Вы добавили себе очко ваш счет: " + humenScore + " и счет компьютера: " + computerScore + " ")
-        alert("Вы добавили себе очко ваш счет: " + humenScore + " и счет компьютера: " + computerScore + " ")
+        return "computerWin" 
     }
 }
 
 function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
-        playRound()
+         const getResultWin = playRound()
+         if (getResultWin == "humanWin") {
+            humanScore++
+            alert("Вы победили ваш счет:" + humanScore + " и счет компьютера: " + computerScore + " ")
+         }
+         else if (getResultWin == "computerWin") {
+            computerScore++
+            alert("Вас унизил компьютер, ваш счет: " + humanScore + " и счет компьютера: " + computerScore + " ")
+         } 
     }
-    if (humenScore > computerScore) {
-        alert("Вы победили ваш счет:" + humenScore + " и счет компьютера: " + computerScore + " ")
+    if (humanScore > computerScore) {
+        alert("Вы победили ваш счет: " + humanScore + " и счет компьютера: " + computerScore + " ")
     }
     else {
-        alert("Вы проиграли ваш счет:" + humenScore + " и счет компьютера: " + computerScore + " ")
+        alert("Вы проиграли ваш счет: " + humanScore + " и счет компьютера: " + computerScore + " ")
     }
     if (confirm("Вы хотите еще раз поиграть ")) {
-        humenScore = 0;
+        humanScore = 0;
         computerScore = 0;
         playGame();
     }
